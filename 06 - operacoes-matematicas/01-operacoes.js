1176d8a69bb7da0d4947f5095ab4bc8f15f32783
Felipe Franco <felipe@fabricadecodigo.com>
2022-04-01 08:39:23 -0300

    operações

---
+++ 06 - operacoes-matematicas/01-operacoes.js
@@ -0,0 +1,11 @@
+console.log(`Maior valor: ${Math.max(4, 5)}`);
+console.log(`Menor valor: ${Math.min(4, 5)}`);
+console.log(`Raiz quadrada: ${Math.sqrt(4)}`);
+console.log(`Valor positivo: ${Math.abs(-4)}`);
+console.log(`Menor inteiro ${Math.floor(5.5)}`)
+console.log(`Menor inteiro ${Math.floor(-5.5)}`)
+console.log(`Valor arredondado: ${Math.round(2.4)}`); // 2
+console.log(`Valor arredondado: ${Math.round(2.5)}`); // 3
+console.log(`Valor truncado: ${Math.trunc(2.667)}`);
+
+console.log(Math.PI)
